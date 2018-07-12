59f69c114887560b0dfc991f97ad4939d015ef0c
ASSOUNE Mike <massoune@nexity.fr>
2018-07-12 18:13:42 +0200

    'init'

---
+++ src/components/login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+import Input from 'components/form/input/Input'
+
+const Login = () => (
+  <View>
+    <Input/>
+  </View>
+)
+
+export default Login
